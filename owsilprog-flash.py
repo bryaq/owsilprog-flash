54e65b4f0a6414e6eb15a66c0c49137ac5f41d76
Roman Zhukov <rzhukov@gmail.com>
2017-04-20 23:58:27 +0500

    read to hex support; verify fixed

--- owsilprog-flash.py
+++ owsilprog-flash.py
@@ -32,7 +32,8 @@ def update_progress(progress):
 VERSION = '0.1.1'
 
 parser = argparse.ArgumentParser()
-parser.add_argument("file", help="name of hex file to be flashed")
+parser.add_argument("file", help="name of hex file")
+parser.add_argument("-r", "--read", type=int, help="number of bytes to read")
 parser.add_argument("-p", "--port", default=None, help="connected COM Port")
 parser.add_argument("-b", "--baud", default=38400, type=int, help="COM Port baudrate [default: 38400]")
 args = parser.parse_args()
@@ -53,8 +54,11 @@ except:
   closeall()
   
 try:
-  file = open(args.file, 'r')
-  size_file = os.path.getsize(args.file)
+  if args.read:
+    file = open(args.file, 'w')
+  else:
+    file = open(args.file, 'r')
+    size_file = os.path.getsize(args.file)
 except:
   print('Cannot open file {0}'.format(args.file))
   closeall()
@@ -72,67 +76,96 @@ if response.find('tok'.encode()) != -1:
   response = flasher.read(20)
   i = response.find('dok'.encode())
   device_id = response[i+3:i+5].decode()
-  sys.stdout.write('Device ID: 0x{0}\nConfirm to Flash {1} byte [y/n]: '.format(device_id, size_file))
- 
-  response = input()
- 
-  if response.lower() == 'y':
-    
-    # Erase process
-    print('\nErase...')
-    flasher.write('e'.encode())
-    flasher.flush()
-    isOK = False
-    i = 0
-    while not isOK:
-      b = flasher.read(1)
-      if b == 'o':
-        i = 1
-      if b == 'k' and i == 1:
-        isOK = True
-    flasher.read(2)
-    
-    # Flash process
-    print('\nFlashing...')
-    sendByte = 0
+  sys.stdout.write('Device ID: 0x{0}\n'.format(device_id))
+  if args.read != None:
+    n = args.read
+    addr = 0
+    print('\nReading...')
     update_progress(0)
-    for line in file:
-      line = line.strip()
-      sendByte = sendByte + len(line)
-      if line[0:3] != ':00':
-        flasher.write(('bw'+line.strip()).encode())
-        flasher.flush()
-      flasher.read(len(line) + 6)
-      update_progress(sendByte*100/size_file)
-    update_progress(100)
-    
-    # reset serial IO
-    flasher.flush()
-    flasher.read(10)
-    
-    # verify process
-    print('\nVerify...')
-    sendByte = 0
-    update_progress(0)
-    file.seek(0)
-    for line in file:
-      line = line.strip()
-      sendByte = sendByte + len(line)
-      if line[0:3] != ':00':
-        flasher.write(('br'+line[0:7]).encode())
-        flasher.flush()
-        response = flasher.read(11 + len(line))
-        if response[9:11] != 'ok' or response[11:end-2] != line:
-          print('error')
-          closeall();
-      update_progress(sendByte*100/size_file)
-    update_progress(100)
-    
+    while n > 0:
+      if n > 16:
+        len = 16
+      else:
+        len = n
+      flasher.write('br:{:02X}{:04X}'.format(len, addr).encode())
+      flasher.flush()
+      response = flasher.read(11+9+len*2+4)
+      if response[9:11] != 'ok':
+        print('error')
+        file.write(':00000001FF\n'.encode())
+        closeall();
+      file.write(response[11:11+9+len*2+3].encode())
+      n -= len
+      addr += len
+      update_progress(addr * 100 / args.read)
+    file.write(':00000001FF\n'.encode())
     flasher.write('r'.encode())
     flasher.flush()
     flasher.read(5)
     print('\nSuccess\n[press any key to exit]')
     response = input()
+  else:
+    sys.stdout.write('Confirm to Flash[y/n]: ')
+   
+    response = input()
+   
+    if response.lower() == 'y':
+      
+      # Erase process
+      print('\nErase...')
+      flasher.write('e'.encode())
+      flasher.flush()
+      isOK = False
+      i = 0
+      while not isOK:
+        b = flasher.read(1)
+        if b == 'o':
+          i = 1
+        if b == 'k' and i == 1:
+          isOK = True
+      flasher.read(2)
+      
+      # Flash process
+      print('\nFlashing...')
+      sendByte = 0
+      update_progress(0)
+      for line in file:
+        line = line.strip()
+        sendByte = sendByte + len(line)
+        if line[0:3] != ':00':
+          flasher.write(('bw'+line.strip()).encode())
+          flasher.flush()
+        flasher.read(len(line) + 6)
+        update_progress(sendByte*100/size_file)
+      update_progress(100)
+      
+      # reset serial IO
+      flasher.flush()
+      flasher.read(10)
+      
+      # verify process
+      print('\nVerify...')
+      sendByte = 0
+      update_progress(0)
+      file.seek(0)
+      for line in file:
+        line = line.strip()
+        sendByte = sendByte + len(line)
+        if line[0:3] != ':00':
+          flasher.write(('br'+line[0:7]).encode())
+          flasher.flush()
+          response = flasher.read(11 + len(line) + 2)
+          if response[9:11] != 'ok' or response[11:-2] != line:
+            print('error')
+            closeall();
+        update_progress(sendByte*100/size_file)
+      update_progress(100)
+      
+      flasher.write('r'.encode())
+      flasher.flush()
+      flasher.read(5)
+      print('\nSuccess\n[press any key to exit]')
+      response = input()
 else:
   print('Port {0} Error'.format(args.port))
 closeall()
